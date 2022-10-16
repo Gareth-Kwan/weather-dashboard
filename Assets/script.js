27e2c21edd7f89678519bb25a14a189855165129
GK <gareth0201@gmail.com>
2022-10-16 18:42:05 -0400

    Fixed a bug with the weather icon in JS

--- Assets/script.js
+++ Assets/script.js
@@ -8,10 +8,21 @@ let dashWind = document.querySelector(".dashboard_wind");
 let dashHumidity = document.querySelector(".dashboard_humidity");
 let dashIcon = document.querySelector(".dashboard_icon");
 let fiveDayForecast = document.querySelector(".fiveDayForecast");
+let history = document.querySelector(".history");
+let historyUl = document.querySelector(".historyUl");
+let cityHistoryList = [];
 
 //Click event function when you click search
 searchButton.addEventListener("click", function () {
   let cityName = searchInput.value;
+  let cityHistory = JSON.parse(localStorage.getItem("cityHistory")) || [];
+
+  //check if cityName is already a city inside the cityHistory array
+  cityHistory.push(cityName);
+  localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+  renderLocalStorageCityName();
+
+  //API Variables
   let APIKey = "56fd51a87db5dd7df7ddaa8c41913fa4";
   let weatherAPI =
     "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey + "&units=metric";
@@ -30,7 +41,7 @@ searchButton.addEventListener("click", function () {
         // console.log(data.list[0].weather[0].icon);
         // console.log(data.list[0].wind.speed);
         // console.log(data.list[0].main.humidity);
-        dashCityName.textContent = "City: " + data.city.name + " (" + data.list[0].dt_txt + ")";
+        dashCityName.textContent = "City: " + data.city.name + " (" + data.list[0].dt_txt.split(" ")[0] + ")";
         dashDescription.textContent = "Description: " + data.list[0].weather[0].description;
         dashTemp.textContent = "Temperature: " + data.list[0].main.temp + "°C";
         dashWind.textContent = "Wind Speed: " + data.list[0].wind.speed + "Km/h";
@@ -38,7 +49,7 @@ searchButton.addEventListener("click", function () {
         let weatherDashBoard = document.querySelector(".weatherDashBoard");
         weatherDashBoard.style.border = "2px solid black";
         let dashWeatherPicture = document.createElement("img");
-        dashWeatherPicture.src = data.list[0].weather[0].icon;
+        dashWeatherPicture.src = `http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
         dashIcon.appendChild(dashWeatherPicture);
       });
   }
@@ -51,7 +62,6 @@ searchButton.addEventListener("click", function () {
       })
       .then(function (data) {
         days = [0, 7, 15, 23, 31];
-
         days.forEach(function (i) {
           // console.log(data);
           // console.log(data.list[i].main.temp);
@@ -63,12 +73,15 @@ searchButton.addEventListener("click", function () {
           let forecastTemp = document.createElement("p");
           let forecastWind = document.createElement("p");
           let forecastHumidity = document.createElement("p");
-          forecastDate.textContent = "Date: " + data.list[i].dt_txt;
+          let forecastWeatherPicture = document.createElement("img");
+          forecastDate.textContent = "Date: " + data.list[i].dt_txt.split(" ")[0];
           forecastTemp.textContent = "Temperature: " + data.list[i].main.temp + "°C";
           forecastWind.textContent = "Wind Speed: " + data.list[i].wind.speed + "Km/h";
           forecastHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
           divEl.setAttribute("class", "fiveDayForecastCard");
+          forecastWeatherPicture.src = `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
           divEl.appendChild(forecastDate);
+          divEl.appendChild(forecastWeatherPicture);
           divEl.appendChild(forecastTemp);
           divEl.appendChild(forecastWind);
           divEl.appendChild(forecastHumidity);
@@ -76,7 +89,28 @@ searchButton.addEventListener("click", function () {
         });
       });
   }
+
   //Invoke Functions
   renderWeatherDashboard();
   renderFiveDayForecast();
 });
+
+//Function to render local storage
+function renderLocalStorageCityName() {
+  // get the data
+  let cityHistory = JSON.parse(localStorage.getItem("cityHistory")) || [];
+  //empty the html element container of the button list items
+  if (cityHistory.length == 0) {
+    console.log("no searches yet");
+  } else {
+    for (let i = 0; i < cityHistory.length; i++) {
+      let historyList = document.createElement("li");
+      historyList.textContent = cityHistory[i];
+      historyList.setAttribute("class", "historyList");
+      //create an event listener and use the same function on the on the top
+      historyUl.appendChild(historyList);
+    }
+  }
+}
+
+renderLocalStorageCityName();
